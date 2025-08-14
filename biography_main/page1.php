8fbfefa835831e5d0c59bef403c826a0f8de15df
JOHN ROLAND LOPEZ OCTAVIO <2300380@cc.usls.edu>
2025-08-14 08:14:19 +0800

    chore: bootstrap

--- biography_main/page1.php
+++ biography_main/page1.php
@@ -44,7 +44,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 </head>
-<body>
+<body class='d-flex flex-column gap-4'>
     <!--
         NOTE: Parenthesis can be omitted in PHP new versions (e.g: include 'file.php')
         include('file.php') -- every include statement, the component is added
@@ -62,16 +62,20 @@
             <p id="date"></p>
         </div>
     </div>
-    <h1><?="$Firstname $Lastname "; ?></h1>
-    <img src="https://avatars.githubusercontent.com/u/121501140?v=4" alt="Image of Jhong" width="200" id="myImage"/>
-    <button onclick="hideImage()">HIDE</button>
-    <button onclick="showImage()">SHOW</button>
-    <blockquote>
+    <h1 class="text-center"><?="$Firstname $Lastname "; ?></h1>
+    <div class="container d-flex flex-column gap-3">
+        <img src="https://avatars.githubusercontent.com/u/121501140?v=4" alt="Image of Jhong" id="myImage" class="w-25 h-25 rounded-circle mx-auto"/>
+        <div class="container d-flex gap-4 w-25 justify-content-center">
+            <button onclick="hideImage()">HIDE</button>
+            <button onclick="showImage()">SHOW</button>
+        </div>
+    </div>
+    <blockquote class='w-50 mx-auto'>
         <strong>
             "Bigay mo sakin saber tol papatayin ko si wise level 4. Gusto ko lang sabihin na hindi madali yung ginagawa namin dito sa Showtime. Sorry direk sorry... Sample! Sample!"
         </strong> <br/> - Anonymous
     </blockquote>
-    <h2>Third Year Computer Science Student <br/> University of St. La Salle - Bacolod</h2>
+    <h2 class='mx-auto w-50 text-center'>Third Year Computer Science Student <br/> University of St. La Salle - Bacolod</h2>
     <?php include 'about-table.php' ?>
 </body>
 </html>
